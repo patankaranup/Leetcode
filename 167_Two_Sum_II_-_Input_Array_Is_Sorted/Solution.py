3df863d7e50e87fffc76f1e2326ee7a801b7117b
Anup Patankar <anantpatankar1@icloud.com>
2022-12-20 00:02:42 +0530

    Two sum ||

---
+++ 167_Two_Sum_II_-_Input_Array_Is_Sorted/Solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l = 0
+        r = len(numbers)-1
+        while (l < r):
+            curSum = numbers[l]+numbers[r]
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l+1, r+1]
