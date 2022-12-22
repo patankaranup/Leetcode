0bce8d0f89667cc46ed166071fa263bc992046b0
Anup Patankar <anantpatankar1@icloud.com>
2022-12-23 00:35:33 +0530

    Valid Anagram

---
+++ 242_Valid_Anagram/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for key in countS:
+            if countS[key] != countT.get(key, 0):
+                return False
+
+        return True
