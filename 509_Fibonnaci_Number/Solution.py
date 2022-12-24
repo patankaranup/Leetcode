9e027cdbb6b79eb21bdfabc466aeb9db9d34084e
Anup Patankar <anantpatankar1@icloud.com>
2022-12-25 01:06:51 +0530

    Fibonnaci Number (recursion and Memoization)

---
+++ 509_Fibonnaci_Number/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    
+    def fib(self, n, memo={}):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n in memo:
+            return memo[n]
+        if n == 0:
+            return 0
+        if n ==1 :
+            return 1
+        memo[n] = self.fib(n-1,memo)+self.fib(n-2,memo)
+        return memo[n]
+    
