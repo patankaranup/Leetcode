54150efe0206df456fe0d28902026c7ba84bc772
Anup Patankar <anantpatankar1@icloud.com>
2022-12-25 03:05:16 +0530

    Linked List Cycle 2 (hare and tortoise)

---
+++ 142_Link_List_Cycle_II/Solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while head != slow:
+                    slow = slow.next
+                    head = head.next
+                return head
+        else:
+            return None
+        
+        
+        
