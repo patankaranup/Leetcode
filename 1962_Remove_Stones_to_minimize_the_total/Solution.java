8cc96401cc338bfc75b1704ce5a5beb928ac93a5
Anup Patankar <anantpatankar1@icloud.com>
2022-12-28 20:47:22 +0530

    Remove stone to minimize the total (greedy approach priority queue max heap)

---
+++ 1962_Remove_Stones_to_minimize_the_total/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        int sum  = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>( (a,b)-> 
+b-a );
+
+        for (int i = 0; i<piles.length;i++){
+            pq.add(piles[i]);
+        }
+        int t = 0;
+        while (t<k){
+            int c = pq.poll();
+            int r = Math.floorDiv(c,2);
+            pq.offer(c-r);
+            t++;
+        }
+        while(!pq.isEmpty()){
+            sum+=pq.poll();
+        }
+        return sum;
+        
+    }
+}
