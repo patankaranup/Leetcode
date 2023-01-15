3c88f74e24429aaae369ccf02f0f4b319ee28f86
Anup Patankar <anantpatankar1@icloud.com>
2023-01-16 00:15:33 +0530

    Binary Search Peak Element in the array

---
+++ 162_Find_Peak_Element/Solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length -1 ;
+
+        while (start<end){
+            int mid = start+(end-start)/2;
+            
+            if (nums[mid]>nums[mid+1]){
+                // we are in decreasing part of the array this may be the 
+answer but look at left half of the array
+                // this is may be the answer that is why we are not saying 
+end = mid -1
+                end = mid;
+
+            }
+            else{
+                // we are in the ascending part of the array
+                start = mid+1; // beacuase we know that the mid + 1 
+elemnet is grater than the mid element hence we ignore the mid 1
+                // and at the end mid will be pointing towards the maximum 
+value because of the above 2 checks 
+                // start and end are always trying to check the max 
+element in the above 2 checks 
+                // hence when the loop breaks they will be pointing to the 
+same element that is the maximum one 
+            }
+            // we can return end also as both are pointing towards same 
+index
+        }
+
+        return start; 
+        
+        
+    }
+}
+
+
