c5c7696a5c8791cf8d389dd1108ac92bed32acab
patankaranup <108407972+patankaranup@users.noreply.github.com>
2023-03-25 23:52:14 +0530

    Unrechable Pairs in undirected graph (DSU)

---
+++ 2316_Count_Unreachable_Pairs_of_Nodes_in_A_Undirected_Graph/Solution.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) 
+            return;
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        
+        for(int i = 0; i<n; i++) {
+            parent[i] = i;
+        }
+        //make components
+        for(auto &vec : edges) {
+            
+            int u = vec[0];
+            int v = vec[1];
+            
+            Union(u, v);
+            
+        }
+       // parent and size of components
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i<n; i++) {
+            int father = find(i);
+            mp[father]++;
+        }
+        
+        long long result = 0;
+        long long remainingNodes = n;
+        
+        for(auto &it : mp) {
+            
+            long long size = it.second;
+            
+            result += (size) * (remainingNodes-size);
+            
+            remainingNodes -= size;
+            
+        }
+        
+        return result;
+    }
+};
