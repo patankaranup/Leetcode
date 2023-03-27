6761f6fbc74075093fef85e801a8a98a849be6c2
patankaranup <108407972+patankaranup@users.noreply.github.com>
2023-03-27 22:36:31 +0530

    Minimum Sum Path (Recursion + Memo and Bottom Up Approach)

---
+++ 64_Minimum_Path_Sum/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int t[201][201];
+    int solve(vector<vector<int>>& grid, int i, int j, int m, int n){
+        if(i==m-1 && j==n-1){
+            // last cell
+            return grid[i][j];
+        }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+        if(i==m-1){ 
+            // last row 
+            return t[i][j] = grid[i][j] + solve(grid,i,j+1,m,n);
+        } else if(j==n-1){
+            // last col
+            return t[i][j] = grid[i][j] + solve(grid,i+1,j,m,n);
+        } else {
+            // explore both
+            return t[i][j] = grid[i][j] + min(solve(grid,i+1,j,m,n),solve(grid,i,j+1,m,n));
+        }
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        memset(t,-1,sizeof(t));
+
+        return solve(grid,0,0,m,n);
+    }
+};
