2bd23cf8518d1c8627b50b5d217137e7fe365a3a
patankaranup <108407972+patankaranup@users.noreply.github.com>
2023-05-05 23:51:35 +0530

    max number of vowels in given substr of length k (Sliding Window)

---
+++ 1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    int maxVowels(string s, int k) {
+        int n = s.length();
+        int i = 0, j = 0;
+        int maxV = 0;
+        int count = 0;
+        while(j<n){
+            if(isVowel(s[j])){
+                count++;
+            } 
+            if(j - i + 1 == k){
+                maxV = max(count,maxV);
+                if(isVowel(s[i])){
+                    count--;
+                }
+                i++;
+            }
+            j++;
+        }
+        return maxV;
+    }
+};
