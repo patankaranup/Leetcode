67592cc26c52634a6a6332f9cf6af32e39031715
patankaranup <108407972+patankaranup@users.noreply.github.com>
2023-05-07 23:46:31 +0530

    Longest Valid Obstacle Course at Each Position (Longest Increasing Subsequence)

---
+++ 1964_Find_the_Longest_Valid_Obstacle_Course_at_Each_Position/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        int n = obstacles.size();
+        vector<int> LIS;
+        vector<int> result(n);
+        for(int i = 0; i<n; i++){
+            int idx  = upper_bound(begin(LIS),end(LIS), obstacles[i]) - begin(LIS);
+            if(idx==LIS.size()){
+                LIS.push_back(obstacles[i]);
+            } else {
+                LIS[idx] = obstacles[i];
+                // idx+1 len of subsequence
+                // ending element of obstacles[i]
+            }
+            result[i] = idx+1;
+        }
+        return result;
+    }
+};
