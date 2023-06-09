1a69079340058c402aea508cfa5802c1f21f2212
patankaranup <108407972+patankaranup@users.noreply.github.com>
2023-06-10 02:59:29 +0530

    Find Smallest Letter Greater Than Target (Using c++ stl)

---
+++ 744_Find_Smallest_letter_Greater_than_target/Solution1.cpp
@@ -0,0 +1,11 @@
+class Solution1 {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int idx = upper_bound(begin(letters),end(letters),target) - begin(letters);
+        if(idx==letters.size()){
+            return letters[0];
+        } else{
+            return letters[idx];
+        }
+    }
+};
