b60508ec4f97d665653c780eff820f3de1a4c544
Anup <108407972+patankaranup@users.noreply.github.com>
2023-06-15 16:49:53 +0000

    Maximum Level S of a binary tree (Using dfs traversal)

---
+++ 1161_Maximum_Level_Sum_of_a_Binary_Tree/Solution1.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution1 {
+public:
+    map<int, int> mp; // level -> sum
+    void DFS(TreeNode* root, int level){
+        if(!root){
+            return;
+        }
+        mp[level] += root->val;
+        DFS(root->left,level+1);
+        DFS(root->right,level+1);
+    }
+    int maxLevelSum(TreeNode* root) {
+        mp.clear();
+        DFS(root,1);
+        int maxSum = INT_MIN;
+        int resultLevel = 0;
+        for(auto &it : mp){
+            int level = it.first;
+            int sum = it.second;
+            if(sum>maxSum){
+                maxSum = sum;
+                resultLevel = level;
+            }
+        }
+        return resultLevel;
+    }
+};
