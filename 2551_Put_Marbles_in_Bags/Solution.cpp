cb511d9351bbf7d73aa703c55df57039302457d4
Anup <108407972+patankaranup@users.noreply.github.com>
2023-07-08 20:18:23 +0000

    Put Marbles in Bags

---
+++ 2551_Put_Marbles_in_Bags/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        int m = n-1;
+        vector<int> pairSum(m,0);
+        for(int i=0; i<n-1; i++){
+            pairSum[i] = weights[i]+weights[i+1];
+        }
+        sort(begin(pairSum),end(pairSum));
+        long long maxSum = 0;
+        long long minSum = 0;
+        for(int i=0; i<k-1; i++){
+            minSum += pairSum[i];
+            maxSum += pairSum[m-1-i];
+        }
+        return maxSum-minSum;
+    }
+};
