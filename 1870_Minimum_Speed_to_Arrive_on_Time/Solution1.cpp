a2bc1c6d2818e585f85590e072fc7f8346ea387e
Anup <108407972+patankaranup@users.noreply.github.com>
2023-07-26 19:25:43 +0000

    Minimum Speed to Arrive on Time (Simple Binary Search)

---
+++ 1870_Minimum_Speed_to_Arrive_on_Time/Solution1.cpp
@@ -0,0 +1,32 @@
+class Solution1 {
+public:
+    double h;
+    int n;
+    double timeToTravel(vector<int>& dist, double speed){
+        double time = 0.0;
+        int i = 0;
+        for(; i<n-1; i++){
+            double t = (double)dist[i]/(double)speed;
+            time += ceil(t);
+        }
+        time += dist[i]/speed;
+        return time;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        h = hour;
+        n = dist.size();
+        int l = 1;
+        int r = 1e7;
+        int result = -1;
+        while(l <= r){
+            int mid_speed = l + (r - l)/2;
+            if(timeToTravel(dist,mid_speed) <= hour){
+                result = mid_speed;
+                r = mid_speed - 1;
+            } else {
+                l = mid_speed + 1;
+            }
+        }
+        return result;
+    }
+};
