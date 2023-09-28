a258bc96431531d4533fdbcb53ff8c7ed477225d
Anup <108407972+patankaranup@users.noreply.github.com>
2023-09-28 21:46:05 +0000

    Sort Array By Parity (Array Sorting using comparator )

---
+++ 905. Sort Array By Parity/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    static bool comparator(int &a,int &b){
+        return a%2 < b%2;
+    }
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(begin(nums),end(nums),comparator);
+        return nums;
+    }
+};
