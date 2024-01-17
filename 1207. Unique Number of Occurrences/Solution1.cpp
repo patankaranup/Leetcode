fcc491585c2f38f3602effcb27d07ed8a9b3f66e
Anup <anantpatankar1@icloud.com>
2024-01-17 21:54:14 +0530

    Unique Number of Occurrences (Using Vector to store freq of individual elements)

---
+++ 1207. Unique Number of Occurrences/Solution1.cpp
@@ -0,0 +1,16 @@
+class Solution1 {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> vec(2001,0);
+        for(int &num:arr) {
+            vec[num+1000]++;
+        }
+        sort(begin(vec),end(vec));
+        for(int i=1; i<2001; i++){
+            if(vec[i] != 0 && vec[i] == vec[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
