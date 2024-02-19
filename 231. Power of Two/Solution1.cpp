24a3d62df712d140a74349751e3dab57ffd9cae4
Anup <anantpatankar1@icloud.com>
2024-02-19 22:40:10 +0530

    Power of Two (Using STL)

---
+++ 231. Power of Two/Solution1.cpp
@@ -0,0 +1,10 @@
+class Solution1 {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        int count = __builtin_popcount(n);
+        return count == 1;
+    }
+};
