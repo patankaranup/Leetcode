6b7aadcf28a9698b84d9a9e817092e1245019541
Anup <anantpatankar1@icloud.com>
2024-02-28 22:54:08 +0530

    Find Bottom Left Tree Value (BFS)

---
+++ 513. Find Bottom Left Tree Value/Solution1.cpp
@@ -0,0 +1,26 @@
+class Solution1 {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> que;
+        
+        int bottomLeft;
+        que.push(root);
+        
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            
+            bottomLeft = node->val;
+            
+            if(node->right) {
+                que.push(node->right);
+            }
+            if(node->left) {
+                que.push(node->left);
+            }
+        }
+        
+        return bottomLeft;
+    }
+};
+
