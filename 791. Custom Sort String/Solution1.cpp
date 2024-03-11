825d4884ca08288ce99a2bdf004a4704e7ff29a5
Anup <anantpatankar1@icloud.com>
2024-03-11 22:23:34 +0530

    Custom Sort String (Comparator)

---
+++ 791. Custom Sort String/Solution1.cpp
@@ -0,0 +1,17 @@
+class Solution1 {
+public:
+    string customSortString(string order, string str) {
+        vector<int> index(26, -1);
+        
+        for(int i = 0; i<order.length(); i++)
+            index[order[i]-'a'] = i;
+        
+        
+        auto lambda = [&index](char &ch1, char &ch2) {
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
+        
+        sort(begin(str), end(str), lambda);
+        return str;
+    }
+};
