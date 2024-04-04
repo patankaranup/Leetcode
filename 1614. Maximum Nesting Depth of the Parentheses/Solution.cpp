d21162379e4393cf50f1e4e4d5834ba7437c8cb2
Anup <anantpatankar1@icloud.com>
2024-04-04 23:38:47 +0530

    Maximum Nesting Depth of the Parentheses (count open paranthesis)

---
+++ 1614. Maximum Nesting Depth of the Parentheses/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int openBrackets = 0;
+        int result = 0;
+        for(auto &ch : s){
+            if(ch == '('){
+                openBrackets++;
+            }
+            if(ch == ')'){
+                openBrackets--;
+            }
+            result = max(result,openBrackets);
+        }
+        return result;
+    }
+};
